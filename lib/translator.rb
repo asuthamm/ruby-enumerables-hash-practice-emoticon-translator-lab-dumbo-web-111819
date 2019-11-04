13a4078d0e873f23e25bd918ef0eec516ccafecd
Alex Suthammanont <asuthamm@gmail.com>
2019-11-04 00:48:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,8 +23,7 @@ def get_japanese_emoticon(file_path,jp_emoji)
   emoji =  hash[:get_emoticon][jp_emoji]
   
   return emoji if !!emoji 
-  return "Sorry, that emoticon was not found"  
-  
+  "Sorry, that emoticon was not found"  
 end
 
 
